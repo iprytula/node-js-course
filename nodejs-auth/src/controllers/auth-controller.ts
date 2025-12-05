82bde58dfcfd20a87ac1c7612f74d5a31bb4d6c5
Iurii Prytula <iurii.prytula@gmail.com>
2025-12-05 13:18:20 +0100

    feat: add change password and logout functionality to auth routes

--- nodejs-auth/src/controllers/auth-controller.ts
+++ nodejs-auth/src/controllers/auth-controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import User, { IUser } from "../models/User";
 import { StatusCodes } from "http-status-codes";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 interface RegisterRequest extends Request {
   body: {
@@ -28,6 +28,16 @@ interface AuthResponse extends Response {
     token?: string;
     error?: any;
   }): this;
+}
+
+export interface ChangePasswordRequest extends Request {
+  body: {
+    currentPassword: string;
+    newPassword: string;
+  };
+  userInfo?: JwtPayload & {
+    role: string;
+  };
 }
 
 const registerUser = async (req: RegisterRequest, res: AuthResponse) => {
@@ -150,4 +160,47 @@ const loginUser = async (req: LoginRequest, res: AuthResponse) => {
   }
 };
 
-export { registerUser, loginUser };
+const changePassword = async (req: ChangePasswordRequest, res: Response) => {
+  try {
+    const loggedInUser = await User.findById(req.userInfo!.id);
+    if (!loggedInUser) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        message: "Unauthorized"
+      });
+    }
+
+    const { currentPassword, newPassword } = req.body;
+
+    const isCurrentPasswordValid = await bcrypt.compare(currentPassword, loggedInUser.password);
+    if (!isCurrentPasswordValid) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        message: "Current password is incorrect"
+      });
+    }
+    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+    loggedInUser.password = hashedNewPassword;
+    await loggedInUser.save();
+    return res.status(StatusCodes.OK).json({
+      message: "Password changed successfully"
+    });
+  } catch (error) {
+    console.error("Change password error:", error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Error during password change",
+      error: process.env.NODE_ENV === "development" ? error : {}
+    });
+  }
+};
+
+const logOutUser = (req: Request, res: Response) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict"
+  });
+  return res.status(StatusCodes.OK).json({
+    message: "Logout successful"
+  });
+};
+
+export { registerUser, loginUser, changePassword, logOutUser };
