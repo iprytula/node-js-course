6f7ee478f6a6f0b8b84a975226c755203b3412d3
Iurii Prytula <iurii.prytula@gmail.com>
2026-01-08 13:04:21 +0100

    feat: enhance product controller with stock status and add product stats and analysis routes

--- mongo-db-intermediate/routes/product-routes.js
+++ mongo-db-intermediate/routes/product-routes.js
@@ -1,7 +1,15 @@
 import express from 'express';
-import { insertSampleProducts } from '../controllers/product-controller.js';
+import {
+  getProductAnalysis,
+  getProductStats,
+  insertSampleProducts,
+} from "../controllers/product-controller.js";
 const router = express.Router();
 
-router.post('/insert-sample-products', insertSampleProducts);
+router.post("/insert-sample-products", insertSampleProducts);
+
+router.get("/stats", getProductStats);
+
+router.get("/analysis", getProductAnalysis);
 
 export default router;
