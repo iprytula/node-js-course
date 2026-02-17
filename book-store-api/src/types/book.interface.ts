ef8e2d40162416877fcd72a640f901e84330e2a7
Iurii <Iurii.Prytula@insert.com.pl>
2026-02-17 10:11:37 +0100

    moved prev course materials to separate directory

--- book-store-api/src/types/book.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface BookInterface {
-  title: string;
-  author: string;
-  price: number;
-  description: string;
-  createdAt?: Date;
-}
--- nodejs-auth/src/routes/file-routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { uploadImage, deleteImage } from "../controllers/file-controller";
-
-const fileRouter = express.Router();
-
-fileRouter.post("/", uploadImage as express.RequestHandler);
-fileRouter.delete("/:id", deleteImage);
-
-export default fileRouter;
