36e13b54a512a78e36178374a808929daff7f154
Jurij <iurii.prytula@gmail.com>
2026-09-07 14:43:40 +0200

    sending json responses

---
+++ 2-node-http/src/04-json-response.ts
@@ -0,0 +1,68 @@
+import http, { type IncomingMessage, type ServerResponse } from "node:http";
+
+const PORT = 3003;
+
+interface User {
+  name: string;
+  email: string;
+}
+
+interface ApiResponse<T> {
+  success: boolean;
+  message: string;
+  data?: T;
+  error?: string;
+}
+
+const users: User[] = [
+  { name: "Iurii", email: "iurii@gmail.com" },
+  { name: "Anna", email: "anna@gmail.com" },
+];
+
+const sendJSON = <T>(
+  res: ServerResponse,
+  statusCode: number,
+  body: ApiResponse<T>,
+): void => {
+  res.statusCode = statusCode;
+
+  res.setHeader("Content-Type", "application/json");
+
+  res.end(JSON.stringify(body));
+};
+
+const server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    const method = req.method ?? "GET";
+    const requestUrl = new URL(req.url ?? "/", `http:${req.headers.host}`);
+    const pathName = requestUrl.pathname;
+
+    if (method === "GET" && pathName === "/") {
+      sendJSON(res, 200, {
+        success: true,
+        message: "server is running",
+        data: {
+          routes: ["GET/users"],
+        },
+      });
+    }
+
+    if (method === "GET" && pathName === "/users") {
+      sendJSON(res, 200, {
+        success: true,
+        message: "users fetched successfully",
+        data: users,
+      });
+    }
+
+    sendJSON(res, 404, {
+      success: false,
+      message: "Route not found",
+      error: `pathname: '${pathName}' does not exist`
+    })
+  },
+);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
